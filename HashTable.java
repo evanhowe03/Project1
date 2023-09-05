2f0b7a3a52c4953c395fa10eec019d5523f96056
evanhowe03 <119268944+evanhowe03@users.noreply.github.com>
2023-09-05 14:16:49 -0400

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -5,128 +5,136 @@ import java.io.*;
  */
 public class HashTable<K, V> {
 
-    private int hashSize;
-    private int size;
-    private Entry[] table;
+	private int hashSize;
+	private int size;
+	private Entry[] table;
 
-    public HashTable(int initialMemorySize, int initialHashSize) {
+	public HashTable(int initialMemorySize, int initialHashSize) {
 
-        int memSize = initialMemorySize;
-        hashSize = initialHashSize;
-        size = 0;
+		int memSize = initialMemorySize;
+		hashSize = initialHashSize;
+		size = 0;
 
-        table = new Entry[hashSize];
+		table = new Entry[hashSize];
 
-    }
+	}
 
+	private int firstHashValue(int key) {
 
-    private int firstHashValue(int key) {
+		int M = hashSize + 1;
+		return key % M;
+	}
 
-        int M = hashSize + 1;
-        return key % M;
-    }
+	private int secondHashValue(int key) {
 
+		int M = hashSize + 1;
+		// System.out.print((((key/M) % (M/2)) * 2) + 1);
+		return (((key / M) % (M / 2)) * 2) + 1;
+	}
 
-    private int secondHashValue(int key) {
+	public void doubleTable() {
 
-        int M = hashSize + 1;
-        // System.out.print((((key/M) % (M/2)) * 2) + 1);
-        return (((key / M) % (M / 2)) * 2) + 1;
-    }
+	}
 
+	public void insert(int key, Seminar value) {
 
-    public void doubleTable() {
+		// doubles the table if the table is more than %50 full
+		if (size >= (hashSize / 2)) {
+			doubleTable();
+			System.out.print("I doubled the table");
+		}
 
-    }
+		int hashing1 = firstHashValue(key);
+		int hashing2 = secondHashValue(key);
+		int i = 0;
 
+		while (table[hashing1] != null && i < table.length) {
 
-    public void insert(int key, Seminar value) {
+			hashing1 = (hashing1 + hashing2) % table.length;
+			i++;
+		}
+		table[hashing1] = new Entry(key, value);
+		size++;
 
-        // doubles the table if the table is more than %50 full
-        if (size >= (hashSize / 2)) {
-            doubleTable();
-            System.out.print("I doubled the table");
-        }
+	}
 
-        int hashing1 = firstHashValue(key);
-        int hashing2 = secondHashValue(key);
-        int i = 0;
+	public void printTable() {
 
-        while (table[hashing1] != null && i < table.length) {
+		System.out.println("\nHash Table");
 
-            hashing1 = (hashing1 + hashing2) % table.length;
-            i++;
-        }
-        table[hashing1] = new Entry(key, value);
-        size++;
+		for (int i = 0; i < size; i++) {
+			if (table[i] != null) {
+				System.out.println(table[i].key + " " + table[i].value.toString());
+			}
+		}
 
-    }
+	}
 
+	public void delete(int key) {
+		// System.out.println("HAHAHA");
+		int index = findIndex(key);
+		// System.out.println(table[index].key + "" +
+		// table[index].value.toString());
+		if (index != -1 && table[index] != null) {
+			table[index].value = null;
 
-    public void printTable() {
+			size--;
 
-        System.out.println("\nHash Table");
+		}
 
-        for (int i = 0; i < size; i++) {
-            if (table[i] != null) {
-                System.out.println(table[i].key + " " + table[i].value
-                    .toString());
-            }
-        }
+	}
 
-    }
+	public void search(int key) {
 
+		int index = findIndex(key);
 
-    public void delete(int key) {
-        // System.out.println("HAHAHA");
-        int index = findIndex(key);
-        // System.out.println(table[index].key + "" +
-        // table[index].value.toString());
-        if (index != -1 && table[index] != null) {
-            table[index].value = null;
+		if (table[index].value != null) {
+			System.out.print(table[index].value + "HAHAHA" + table[index].value.toString());
+		}
 
-            size--;
+	}
 
-        }
+	private int findIndex(int key) {
 
-    }
+		int hashing1 = firstHashValue(key);
+		int hashing2 = secondHashValue(key);
+		int i = 0;
 
+		while (table[hashing1] != null && i < table.length) {
 
-    public void search(int key) {
+			if (table[hashing1].key == key) {
+				return hashing1;
+			}
+			hashing1 = (hashing1 + hashing2) % table.length;
+			i++;
 
-        int index = findIndex(key);
+		}
 
-        if (index != -1) {
-            System.out.print(table[index].value + "" + table[index].value.toString());
-        }
+		return -1; // key not found
 
+	}
 
-    }
+	public void print(String printmythingy) {
+		if (printmythingy.equalsIgnoreCase("blocks")) {
+			System.out.println("Freeblock List:");
+			for (int x = 0; x < hashSize; x++) {
 
+				if (table[x] == null) {
 
-    private int findIndex(int key) {
+					// System.out.print(": " + x + " ");
+				}
+			}
+		}
 
-        int hashing1 = firstHashValue(key);
-        int hashing2 = secondHashValue(key);
-        int i = 0;
+		if (printmythingy.equalsIgnoreCase("hashtable")) {
+			System.out.println("Hashtable:");
+			for (int x = 0; x < size; x++) {
+				System.out.println("ID: " + x + " " + findIndex(x) + "");
+			}
 
-        while (table[hashing1] != null && i < table.length) {
+		}
+		
+	}
 
-            if (table[hashing1].key == key) {
-                return hashing1;
-            }
-            hashing1 = (hashing1 + hashing2) % table.length;
-            i++;
-
-        }
-
-        return -1; // key not found
-
-    }
-
-
-    public void print() {
-
-    }
 
 }
