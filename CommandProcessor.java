2f0cfb5f60487caccb247f3d256388a4e117a2d7
evanhowe03 <119268944+evanhowe03@users.noreply.github.com>
2023-09-05 13:32:10 -0400

    Update CommandProcessor.java

--- CommandProcessor.java
+++ CommandProcessor.java
@@ -1,91 +1,121 @@
-
 import java.io.*;
-import java.util.*;
-import java.util.Scanner;
 
-public class CommandProcessor {
-    
-    public HashTable table; 
+/**
+ * 
+ */
+public class HashTable<K, V> {
 
-    public CommandProcessor(String file, int initialMemorySize, int initialHashSize) {
-        
-        int memSize = initialMemorySize;
-        int hashSize = initialHashSize;
-        
-        table = new HashTable(memSize, hashSize);
+	private int hashSize;
+	private int size;
+	private Entry[] table;
 
-        try {
+	public HashTable(int initialMemorySize, int initialHashSize) {
 
-            Scanner scanner = new Scanner(new File(file));
+		int memSize = initialMemorySize;
+		hashSize = initialHashSize;
+		size = 0;
 
-            while (scanner.hasNext()) {
-                String word = scanner.next();
-        
-                if (word.equals("insert")) {
-                    // System.out.println("YESY");
-                    insert(scanner);
-                } else if (word.equalsIgnoreCase("search")) {
-                    search(scanner);
-                } else if (word.equalsIgnoreCase("delete")) {
-                    delete(scanner);
-                } else {
-                    print(scanner);
-                }
+		table = new Entry[hashSize];
 
-            }
+	}
 
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+	private int firstHashValue(int key) {
 
-    }
+		int M = hashSize + 1;
+		return key % M;
+	}
 
-    public void insert(Scanner scanner) {
+	private int secondHashValue(int key) {
 
-        int id = scanner.nextInt();
-        scanner.nextLine();
-        String title = scanner.nextLine().trim();
-        String date = scanner.next();
-        int length = scanner.nextInt();
-        short x = scanner.nextShort();
-        short y = scanner.nextShort();
-        int cost = scanner.nextInt();
-        scanner.nextLine();
-        String line = scanner.nextLine();
-        String[] words = line.split(" ");
-        
-            
-        String des = scanner.nextLine().trim();
-        // this will call the seminar to create an instance of it
-        //System.out.println("INSERT   ID: " + id + " TITLE: " + title + " DATE: " + date + " LENGTH: " + length + "  X: " + x
-               // + " Y: " + y + " COST: " + cost + " LINE: " + line + " DES: " + des + "");
-        
-        //create a Seminar
-        Seminar mySeminar = new Seminar( id, title, date, length, x,
-            y, cost, words, des);
-        
-        //insert the seminar into the table
-        table.insert(id, mySeminar);
-        
-    }
-    
-    
+		int M = hashSize + 1;
+		// System.out.print((((key/M) % (M/2)) * 2) + 1);
+		return (((key / M) % (M / 2)) * 2) + 1;
+	}
 
-    public void search(Scanner scanner) {
-        int key = scanner.nextInt();
-        table.search(key);
+	public void doubleTable() {
 
-    }
+	}
 
-    public void delete(Scanner scanner) {
-        int delete = scanner.nextInt();
-  
-    }
+	public void insert(int key, Seminar value) {
 
-    public void print(Scanner scanner) {
-        String print = scanner.next();
+		// doubles the table if the table is more than %50 full
+		if (size >= (hashSize / 2)) {
+			doubleTable();
+			System.out.print("I doubled the table");
+		}
 
-    }
+		int hashing1 = firstHashValue(key);
+		int hashing2 = secondHashValue(key);
+		int i = 0;
+
+		while (table[hashing1] != null && i < table.length) {
+
+			hashing1 = (hashing1 + hashing2) % table.length;
+			i++;
+		}
+		table[hashing1] = new Entry(key, value);
+		size++;
+
+	}
+
+	public void printTable() {
+
+		System.out.println("\nHash Table");
+
+		for (int i = 0; i < size; i++) {
+			if (table[i] != null) {
+				System.out.println(table[i].key + " " + table[i].value.toString());
+			}
+		}
+
+	}
+
+	public void delete(int key) {
+		// System.out.println("HAHAHA");
+		int index = findIndex(key);
+		// System.out.println(table[index].key + "" +
+		// table[index].value.toString());
+		if (index != -1 && table[index] != null) {
+			table[index].value = null;
+
+			size--;
+
+		}
+
+	}
+
+	public void search(int key) {
+
+		int index = findIndex(key);
+
+		if (table[index].value != null) {
+			System.out.print(table[index].value + "HAHAHA" + table[index].value.toString());
+		}
+
+	}
+
+	private int findIndex(int key) {
+
+		int hashing1 = firstHashValue(key);
+		int hashing2 = secondHashValue(key);
+		int i = 0;
+
+		while (table[hashing1] != null && i < table.length) {
+
+			if (table[hashing1].key == key) {
+				return hashing1;
+			}
+			hashing1 = (hashing1 + hashing2) % table.length;
+			i++;
+
+		}
+
+		return -1; // key not found
+
+	}
+
+	public void print() {
+
+	}
 
 }
