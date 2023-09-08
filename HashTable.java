66157fb7e175cf930b0de22901186db7bc33dd0a
pchavan2021 <103229699+pchavan2021@users.noreply.github.com>
2023-09-07 22:42:13 -0400

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -1,97 +1,213 @@
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-import org.junit.Test;
-
-import student.TestCase;
+import java.io.*;
 
 /**
- * @author {Your Name Here}
- * @version {Put Something Here}
+ * 
  */
-public class SemManagerTest extends TestCase {
-	/**
-	 * Sets up the tests that follow. In general, used for initialization
-	 */
-	public void setUp() {
-		// Nothing here
-	}
+public class HashTable<K, V> {
 
-	/**
-	 * Get code coverage of the class declaration.
-	 */
-	public void testMInitx() {
-		SemManager sem = new SemManager();
-		// SemManager nullSem = null;
-		// assertNull(nullSem);
-		assertNotNull(sem);
-		// SemManager.main(null);
-	}
+    private int hashSize;
+    private int size;
+    private Entry[] table;
+    private Boolean isDeleted;
 
-	@Test
-	public void testScanner() {
-		String testInput = "word1 word2 word3";
-		Scanner scanner = new Scanner(testInput);
+    public HashTable(int initialMemorySize, int initialHashSize) {
 
-		assertTrue(scanner.hasNext());
-		assertEquals("word1", scanner.next());
-		assertTrue(scanner.hasNext());
-		assertEquals("word2", scanner.next());
-		assertTrue(scanner.hasNext());
-		assertEquals("word3", scanner.next());
+        int memSize = initialMemorySize;
+        hashSize = initialHashSize;
+        size = 0;
 
-		assertFalse(scanner.hasNext());
+        table = new Entry[hashSize];
 
-	}
+    }
 
-	public void testWords() {
-		String test = "insert search delete print";
+    public int getHashSize() {
+        return hashSize;
+    }
 
-		Scanner scanner = new Scanner(test);
+    public int firstHashValue(int key) {
 
-		assertTrue(scanner.hasNext());
-		assertEquals("insert", scanner.next());
-		assertEquals("search", scanner.next());
-		assertEquals("delete", scanner.next());
-		assertEquals("print", scanner.next());
+        int M = hashSize + 1;
+        return key % M;
+    }
 
-	}
+    public int secondHashValue(int key) {
 
-	public void testMain() throws NumberFormatException, Exception {
+        int M = hashSize + 1;
+        // System.out.print((((key/M) % (M/2)) * 2) + 1);
+        return (((key / M) % (M / 2)) * 2) + 1;
+    }
 
-		SemManager sem = new SemManager();
-		String[] args = new String[3];
-		args[0] = "512";
-		args[1] = "4";
-		args[2] = "P1Sample_input.txt";
-		// SemManager sem = new SemManager();
-		SemManager.main(args);
-		String output = systemOut().getHistory();
-		String referenceOutput = readFile("P1Sample_output.txt");
-		// assertFuzzyEquals(referenceOutput, output);
-		assertFuzzyEquals(referenceOutput, output);
+    private void doubleTable() {
+        
+        int oldSize = hashSize;
+        Entry[] oldTable = table;
 
-		assertNotNull(sem);
-	}
+        hashSize = hashSize * 2;
+        table = new Entry[hashSize];
 
-	public String readFile(String name) {
-		String fileFish = "";
-		try {
+        // Rehash all existing entries into the new table
+        for (int i = 0; i < oldSize; i++) {
+            if (oldTable[i] != null) {
+                Entry entry = oldTable[i];
+                int hash1 = firstHashValue(entry.key);
+                int hash2 = secondHashValue(entry.key);
+                
+                int j = 0;
+                while (table[hash1] != null && j < hashSize) {
+                    hash1 = (hash1 + hash2) % hashSize;
+                    j++;
+                }
+                
+                if (j < hashSize) {
+                    table[hash1] = entry;
+                } 
+            }
+            
+        }
 
-			Scanner scan = new Scanner(new File(name));
+     
 
-			while (scan.hasNextLine()) {
-				fileFish += scan.nextLine() + "\n";
-			}
-			scan.close();
-		} catch (FileNotFoundException e) {
+    }
 
-			e.printStackTrace();
-		}
+    public void insert(int key, Seminar value) throws Exception {
+        
+        // System.out.println("SIZE: "+size+" HASHZISE: "+hashSize+"");
+        // doubles the table if the table is more than %50 full
+        if (size >= (hashSize / 2)) {
+            doubleTable();
+            System.out.print("I doubled the table");
+        }
 
-		return fileFish;
-	}
+        int hashing1 = firstHashValue(key);
+        int hashing2 = secondHashValue(key);
+        int i = 0;
+
+        while (table[hashing1] != null && i < hashSize) {
+
+            if (table[hashing1].key == key) {
+                // table[hashing1].value = value;
+                System.out.print("\nInsert FAILED - There is already a record with ID " + key + "");
+                return;
+
+            }
+
+            hashing1 = (hashing1 + hashing2) % table.length;
+            i++;
+        }
+
+        table[hashing1] = new Entry(key, value);
+        size++;
+        System.out.println("Successfully inserted record with ID " + key + "");
+        System.out.println(table[hashing1].value.toString());
+        
+        byte[] x = table[hashing1].value.serialize();
+        System.out.println("Size: " + x.length + "");
+    
+
+        // TEMP FIX SOON
+        // System.out.println("Memory pool expanded to bytes");
+        // System.out.println("Hash Table Expanded to records");
+
+    }
+
+    
+     public void printTable() {
+      
+     // System.out.println("\nHash Table");
+     
+     for (int i = 0; i < size; i++) { if (table[i] != null) {
+         System.out.println(table[i].key + " " + table[i].value.toString()); } }
+      
+     }
+     
+
+    public void delete(int key) {
+        // System.out.println("HAHAHA");
+        int index = findIndex(key);
+        // System.out.println(table[index].key + "" +
+        // table[index].value.toString());
+
+        if (index == -1) {
+
+            System.out.print("\nDelete FAILED -- There is no record with ID " + key);
+
+        } else if (table[index].key == key && table[index] != null) {
+
+            table[index] = null;
+
+            size--;
+            System.out.print("\nRecord with ID " + key + " succesfully deleted from the database");
+
+        }
+
+    }
+
+    public void search(int key) {
+
+        int index = findIndex(key);
+
+        if (index == -1) {
+            System.out.print("\nSearch FAILED -- There is no record with ID " + key);
+
+        } else if (table[index].key == key && table[index] != null) {
+            System.out.println("Found record with ID " + table[index].key + ":");
+            System.out.print(table[index].value.toString());
+
+        }
+    }
+
+    public Seminar searchAndReturn(int key) {
+
+        int index = findIndex(key);
+        if (index == -1) {
+            return null;
+        }
+        // else if (table[index].value != null) {
+        else {
+            return (table[index].value);
+        }
+
+    }
+
+    public int findIndex(int key) {
+
+        int hashing1 = firstHashValue(key);
+        int hashing2 = secondHashValue(key);
+        int i = 0;
+
+        while (table[hashing1] != null && i < hashSize) {
+
+            if (table[hashing1].key == key) {
+                return hashing1;
+            }
+            hashing1 = (hashing1 + hashing2) % table.length;
+            i++;
+
+        }
+
+        return -1; // key not found
+
+    }
+
+
+    public void print(String printmythingy) {
+        if (printmythingy.equalsIgnoreCase("hashtable")) {
+            System.out.print("\nHashtable:");
+            for (int x = 0; x < hashSize; x++) {
+                if (table[x] != null) {
+                    // change to use id not hash value
+                    // System.out.print("\n" + table[x].key + ": " + x + "");
+                    System.out.print("\n" + x + ": " + table[x].key + "");
+
+                }
+            }
+            System.out.print("\ntotal records: " + size);
+        }
+
+        if (printmythingy.equalsIgnoreCase("blocks")) {
+            System.out.print("\nFreeBlockList:");
+
+        }
+
+    }
 }
