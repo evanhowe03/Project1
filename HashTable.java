bac7e4ecd4cf05d2f162c249ce81874ac0f8dbef
pchavan2021 <103229699+pchavan2021@users.noreply.github.com>
2023-09-11 17:02:15 -0400

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -5,216 +5,247 @@ import java.io.*;
  */
 public class HashTable<K, V> {
 
-	private int hashSize;
-	private int size;
-	private Entry[] table;
-	private Boolean isDeleted;
+    private int hashSize;
+    private int size;
+    private Entry[] table;
 
-	public HashTable(int initialMemorySize, int initialHashSize) {
+    public HashTable(int initialMemorySize, int initialHashSize) {
 
-		int memSize = initialMemorySize;
-		hashSize = initialHashSize;
-		size = 0;
+        int memSize = initialMemorySize;
+        hashSize = initialHashSize;
+        size = 0;
 
-		table = new Entry[hashSize];
+        table = new Entry[hashSize];
 
-	}
+    }
 
-	public int getHashSize() {
-		return hashSize;
-	}
 
-	public int firstHashValue(int key) {
+    public int getHashSize() {
+        return hashSize;
+    }
 
-		int M = hashSize;
-		return key % M;
-	}
 
-	public int secondHashValue(int key) {
+    public int firstHashValue(int key) {
 
-		int M = hashSize;
-		// System.out.print((((key/M) % (M/2)) * 2) + 1);
-		return (((key / M) % (M / 2)) * 2) + 1;
-	}
+        int M = hashSize;
+        return key % M;
+    }
 
-	private void doubleTable() {
 
-		int oldSize = hashSize;
-		Entry[] oldTable = table;
+    public int secondHashValue(int key) {
 
-		hashSize = hashSize * 2;
-		table = new Entry[hashSize];
+        int M = hashSize;
+        // System.out.print((((key/M) % (M/2)) * 2) + 1);
+        return (((key / M) % (M / 2)) * 2) + 1;
+    }
 
-		// Rehash all existing entries into the new table
-		for (int i = 0; i < oldSize; i++) {
-			if (oldTable[i] != null) {
-				Entry entry = oldTable[i];
-				int hash1 = firstHashValue(entry.key);
-				int hash2 = secondHashValue(entry.key);
 
-				int j = 0;
-				while (table[hash1] != null && j < hashSize) {
-					hash1 = (hash1 + hash2) % hashSize;
-					j++;
-				}
+    private void doubleTable() {
 
-				if (j < hashSize) {
-					table[hash1] = entry;
-				}
-			}
+        int oldSize = hashSize;
+        Entry[] oldTable = table;
 
-		}
+        hashSize = hashSize * 2;
+        table = new Entry[hashSize];
 
-	}
+        // Rehash all existing entries into the new table
+        for (int i = 0; i < oldSize; i++) {
+            if (oldTable[i] != null) {
+                Entry entry = oldTable[i];
+                int hash1 = firstHashValue(entry.key);
+                int hash2 = secondHashValue(entry.key);
 
-	public void insert(int key, Seminar value) throws Exception {
+                int j = 0;
+                while (table[hash1] != null && j < hashSize) {
+                    if (table[hash1].tomb) {
+                        hash1 = (hash1 + hash2) % hashSize;
+                        return;
+                    }
+                    hash1 = (hash1 + hash2) % hashSize;
+                    j++;
+                }
 
-		// System.out.println("SIZE: "+size+" HASHZISE: "+hashSize+"");
-		// doubles the table if the table is more than %50 full
-		if (size >= (hashSize / 2)) {
-			doubleTable();
-			// System.out.print("I doubled the table");
-		}
+                if (j < hashSize) {
+                    table[hash1] = entry;
+                }
+            }
 
-		int hashing1 = firstHashValue(key);
-		int hashing2 = secondHashValue(key);
-		int i = 0;
+        }
 
-		while (table[hashing1] != null && i < hashSize) {
+    }
 
-			if (table[hashing1].key == key) {
-				// table[hashing1].value = value;
-				System.out.println("Insert FAILED - There is already a record with ID " + key + "");
-				return;
 
-			}
-			if (table[hashing1].tomb) {
-				table[hashing1].key = key;
-			}
-			table[hashing1].tomb = false;
-			hashing1 = (hashing1 + hashing2) % table.length;
-			i++;
-		}
-		// table[hashing1].key = key;
-		table[hashing1] = new Entry(key, value, false);
-		table[hashing1].tomb = false;
-		size++;
-		System.out.println("Successfully inserted record with ID " + key + "");
-		System.out.println(table[hashing1].value.toString());
+    public void insert(int key, Seminar value) throws Exception {
 
-		byte[] x = table[hashing1].value.serialize();
-		System.out.println("Size: " + x.length + "");
+        // System.out.println("SIZE: "+size+" HASHZISE: "+hashSize+"");
+        // doubles the table if the table is more than %50 full
+        if (size >= (hashSize / 2)) {
+            doubleTable();
+            // System.out.print("I doubled the table");
+        }
 
-		// TEMP FIX SOON
-		// System.out.println("Memory pool expanded to bytes");
-		// System.out.println("Hash Table Expanded to records");
+        int hashing1 = firstHashValue(key);
+        int hashing2 = secondHashValue(key);
+        int i = 0;
 
-	}
-	/*
-	 * 
-	 * public void printTable() {
-	 * 
-	 * // System.out.println("\nHash Table");
-	 * 
-	 * for (int i = 0; i < size; i++) { if (table[i] != null) {
-	 * System.out.println(table[i].key + " " + table[i].value.toString()); } }
-	 * 
-	 * }
-	 */
+        while ((table[hashing1] != null && i < hashSize)) {
 
-	public void delete(int key) {
-		// System.out.println("HAHAHA");
-		int index = findIndex(key);
-		// System.out.println(table[index].key + "" +
-		// table[index].value.toString());
+            if (table[hashing1].key == key && table[hashing1].value != null) {
+                // table[hashing1].value = value;
+                System.out.println(
+                    "Insert FAILED - There is already a record with ID " + key
+                        + "");
+                return;
 
-		if (index == -1) {
+            }
+            if (table[hashing1].tomb) {
+                table[hashing1].key = key;
+            }
+          
+            table[hashing1].tomb = false;
+            table[hashing1].value = value;
+            hashing1 = (hashing1 + hashing2) % table.length;
+            i++;
+        }
+        // table[hashing1].key = key;
+        table[hashing1] = new Entry(key, value, false);
+        //table[hashing1].tomb = false;
+        size++;
+        System.out.println("Successfully inserted record with ID " + key + "");
+        System.out.println(table[hashing1].value.toString());
 
-			System.out.println("Delete FAILED -- There is no record with ID " + key);
+        byte[] x = table[hashing1].value.serialize();
+        System.out.println("Size: " + x.length + "");
 
-		} else if (table[index].key == key && table[index] != null) {
+        // TEMP FIX SOON
+        // System.out.println("Memory pool expanded to bytes");
+        // System.out.println("Hash Table Expanded to records");
 
-			table[index].key = 0;
-			table[index].value = null;
-			table[index].tomb = true;
+    }
+    /*
+     * 
+     * public void printTable() {
+     * 
+     * // System.out.println("\nHash Table");
+     * 
+     * for (int i = 0; i < size; i++) { if (table[i] != null) {
+     * System.out.println(table[i].key + " " + table[i].value.toString()); } }
+     * 
+     * }
+     */
 
-			size--;
-			System.out.println("Record with ID " + key + " successfully deleted from the database");
 
-		}
+    public void delete(int key) {
+        // System.out.println("HAHAHA");
+        int index = findIndex(key);
+        // System.out.println(table[index].key + "" +
+        // table[index].value.toString());
 
-	}
+        if (index == -1) {
 
-	public void search(int key) {
+            System.out.println("Delete FAILED -- There is no record with ID "
+                + key);
 
-		int index = findIndex(key);
+        }
+        else if (table[index].key == key && table[index].value != null) {
+            //
 
-		if (index == -1) {
-			System.out.println("Search FAILED -- There is no record with ID " + key);
+            //table[index].key = 0;
+            table[index].value = null;
+            table[index].tomb = true;
 
-		} else if (table[index].key == key && table[index] != null) {
-			System.out.println("Found record with ID " + table[index].key + ":");
-			System.out.println(table[index].value.toString());
+            size--;
+            System.out.println("Record with ID " + key
+                + " successfully deleted from the database");
 
-		}
-	}
+        }
 
-	public Seminar searchAndReturn(int key) {
+    }
 
-		int index = findIndex(key);
-		if (index == -1) {
-			return null;
-		}
-		// else if (table[index].value != null) {
-		else {
-			return (table[index].value);
-		}
 
-	}
+    public void search(int key) {
 
-	public int findIndex(int key) {
+        int index = findIndex(key);
 
-		int hashing1 = firstHashValue(key);
-		int hashing2 = secondHashValue(key);
-		int i = 0;
+        if (index == -1) {
+            System.out.println("Search FAILED -- There is no record with ID "
+                + key);
 
-		while (table[hashing1] != null && i < hashSize) {
+        }
+        else if (table[index].key == key && table[index] != null) {
+            if (table[index].value != null) {
+            System.out.println("Found record with ID " + table[index].key
+                + ":");
+            System.out.println(table[index].value.toString());
+            } else
+            {
+                System.out.println("Search FAILED -- There is no record with ID "
+                    + key);
+                
+            }
 
-			if (table[hashing1].key == key) {
-				return hashing1;
-			}
-			hashing1 = (hashing1 + hashing2) % table.length;
-			i++;
+        }
+    }
 
-		}
 
-		return -1; // key not found
+    public Seminar searchAndReturn(int key) {
 
-	}
+        int index = findIndex(key);
+        if (index == -1) {
+            return null;
+        }
+        else {
+            return (table[index].value);
+        }
 
-	public void print(String printmythingy) {
-		if (printmythingy.equalsIgnoreCase("hashtable")) {
-			System.out.println("Hashtable:");
-			for (int x = 0; x < hashSize; x++) {
-				if (table[x] != null) {
-					// change to use id not hash value
-					// System.out.print("\n" + table[x].key + ": " + x + "");
-					// System.out.println(table[x].tomb);
-					if (table[x].tomb) {
-						System.out.println("" + x + ": TOMBSTONE");
-					} else {
-						System.out.println("" + x + ": " + table[x].key + "");
-					}
+    }
 
-				}
-			}
-			System.out.println("total records: " + size);
-		}
 
-		if (printmythingy.equalsIgnoreCase("blocks")) {
-			System.out.println("FreeBlockList:");
+    public int findIndex(int key) {
 
-		}
+        int hashing1 = firstHashValue(key);
+        int hashing2 = secondHashValue(key);
+        int i = 0;
 
-	}
+        while (table[hashing1] != null && i < hashSize) {
+
+            if (table[hashing1].key == key) {
+                return hashing1;
+            }
+            hashing1 = (hashing1 + hashing2) % table.length;
+            i++;
+
+        }
+
+        return -1; // key not found
+
+    }
+
+
+    public void print(String printmythingy) {
+        if (printmythingy.equalsIgnoreCase("hashtable")) {
+            System.out.println("Hashtable:");
+            for (int x = 0; x < hashSize; x++) {
+                if (table[x] != null) {
+                    // change to use id not hash value
+                    // System.out.print("\n" + table[x].key + ": " + x + "");
+                    // System.out.println(table[x].tomb);
+                    if (table[x].tomb) {
+                        System.out.println("" + x + ": TOMBSTONE");
+                    }
+                    else {
+                        System.out.println("" + x + ": " + table[x].key + "");
+                    }
+
+                }
+            }
+            System.out.println("total records: " + size);
+        }
+
+        if (printmythingy.equalsIgnoreCase("blocks")) {
+            System.out.println("FreeBlockList:");
+
+        }
+
+    }
 }
