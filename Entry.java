f01893ec8aeef025bd1285f2a95e04e037999c89
pchavan2021 <103229699+pchavan2021@users.noreply.github.com>
2023-09-14 21:20:42 -0400

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -1,21 +1,35 @@
+
 /**
- * 
+ * @author Pallavi Chavan pallucha21 and Evan Howe evanhowe03
+ * @version (2023-09-14)
+ * The entry class which is given to the
+ *         hash table.
  */
-import java.util.*;
-import java.io.*;
-/**
- * 
- */
-public class Entry {
-    
+public class Entry{
     public int key;
     public Seminar value;
+    public boolean tomb;
+    public Handle hand;
 
-    public Entry(int k, Seminar v) {
+    /**
+     * 
+     * @param k is the key 
+     *      for the seminar
+     * @param v is the Seminar
+     * @param tombStone 
+     *      the boolean for
+     *      whether the space
+     *      is a tombstone
+     * @param id
+     *      the associated
+     *      handle
+     */
+    public Entry(int k, Seminar v, Boolean tombStone, Handle id) {
         
         this.key = k;
         this.value = v;
-  
+        this.tomb = tombStone;
+        this.hand = id;
     }
-    
+  
 }
